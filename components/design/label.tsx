0f595c9b0043e68b0d59538a1008e7eb13e3cb6d
Patrice Kenmoé <noyau.imperatif@gmail.com>
2025-03-26 07:45:50 +0100

    🟩 Milestone: functional MVP profile form with save & CareOfCarl-inspired layout

---
+++ components/design/label.tsx
@@ -0,0 +1,14 @@
+// components/design/label.tsx
+import * as React from "react";
+
+export const Label = React.forwardRef<
+  HTMLLabelElement,
+  React.LabelHTMLAttributes<HTMLLabelElement>
+>(({ className, ...props }, ref) => (
+  <label
+    ref={ref}
+    className={`text-sm font-medium text-gray-700 ${className || ""}`}
+    {...props}
+  />
+));
+Label.displayName = "Label";
